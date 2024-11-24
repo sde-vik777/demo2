2bb73ff6fa67174bd72b881ff37990149f4a2073
sde-vik777 <sde.vik777@gmail.com>
2024-11-24 22:38:18 +0530

    3rd commit

--- src/main/java/com/jadi/demo2/Demo2Application.java
+++ src/main/java/com/jadi/demo2/Demo2Application.java
@@ -1,13 +1,21 @@
 package com.jadi.demo2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Demo2Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
+
+        while(true){
+            log.info("Hello World");
+        }
     }
 
 }
